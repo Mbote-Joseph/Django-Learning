eba48f47a49111b9cf9f40a52d4fabf30a383579
Mbote-Joseph <mbotejoseph001@gmail.com>
2022-07-01 20:43:43 +0300

    Django Learninng App

--- Learning/urls.py
+++ Learning/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from home import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('home', views.home, name='home'),
+    path('about', views.about, name='about'),
 ]
