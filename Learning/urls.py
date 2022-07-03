57207f0d72ca16b08b410670045387fd33f1165a
Mbote-Joseph <mbotejoseph001@gmail.com>
2022-07-03 22:03:24 +0300

    Django Learninng App

--- Learning/urls.py
+++ Learning/urls.py
@@ -22,5 +22,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('home', views.home, name='home'),
     path('about', views.about, name='about'),
-    path('contact', views.contact, name='contact')
+    path('contact', views.contact, name='contact'),
+    path('email', views.email, name='email')
 ]
