cd6b64065c5a24f426b6ad83e9195f17ec3863df
Mbote-Joseph <mbotejoseph001@gmail.com>
2022-07-05 16:37:30 +0300

    Django Dynamic Templates

--- notes/urls.py
+++ notes/urls.py
@@ -18,5 +18,6 @@ from . import views
 
 urlpatterns = [
     path('home', views.home, name='home'),
+    path('list', views.list, name='list'),
     
 ]
--- notes/views.py
+++ notes/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from datetime import datetime
 
+from .models import Notes
+
 # Create your views here.
 def home(request):
     return render(request, 'home/welcome.html', {'today': datetime.today()})
+
+def list(request):
+    notes = Notes.objects.all()
+    return render(request, 'notes/list.html', {'notes': notes})
